cbfc603ff7913e43127bf269d45eeccc0b543477
Andrey Erdikov <andreyerdikov@MacBook-Air-Andrey.local>
2021-07-17 19:48:02 +0700

    resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,38 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {$} from '@core/dom';
+import {resizeHandler} from '@/components/table/table.resize';
+import {shouldResize} from '@/components/table/table.functions';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown'],
+    })
+  }
+
   toHTML() {
     return createTable(100);
   }
+
+  // onClick() {
+  //   console.log('click');
+  // }
+  //
+  onMousedown(event) {
+    // console.log(event.target.getAttribute('data-resize'));
+    if (shouldResize(event)) {
+      resizeHandler(this.$root, event);
+    }
+  }
+  //
+  // onMousemove() {
+  //   console.log('ms move');
+  // }
+  //
+  // onMouseup() {
+  //   console.log('ms up');
+  // }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,24 +3,31 @@ const CODES = {
   Z: 90,
 }
 
-function toCell(cell) {
+function toCell(cell, col) {
   return `
-    <div class="cell" contenteditable>${cell}</div>
+    <div class="cell" data-col="${col}" contenteditable>${cell}</div>
   `;
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
   return `
-    <div class="column">
+    <div class="column" data-type="resizable" data-col="${index}">
         ${col}
+        <div class="col-resize" data-resize="col"></div>
     </div>
   `;
 }
 
 function createRow(content, index = '') {
+  const resizer = index !== '' ?
+    `<div class="row-resize" data-resize="row"></div>` :
+    '';
   return `
-    <div class="row">
-        <div class="row-info">${index}</div>
+    <div class="row" data-type="resizable">
+        <div class="row-info">
+            ${index}
+            ${resizer}
+        </div>
         <div class="row-data">${content}</div>
     </div>
   `
